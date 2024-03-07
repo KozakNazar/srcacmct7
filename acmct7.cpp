a0ce4f53aa408c16e5a502f9cd5474514f163e29
KozakNazar <55637421+KozakNazar@users.noreply.github.com>
2024-03-07 05:22:47 +0200

    Add files via upload

---
+++ acmct7.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <regex>
+
+/* Example for regular expression:
+(a+b+c+d+e+f+g+h+i+j+k+l+m+n+o+p+q+r+s+t+u+v+w+x+y+z)(a+b+c+d+e+f+g+h+i+j+k+l+m+n+o+p+q+r+s+t+u+v+w+x+y+z)(a+b+c+d+e+f+g+h+i+j+k+l+m+n+o+p+q+r+s+t+u+v+w+x+y+z)
+*/
+std::regex token_re("[a-z]{3}");
+//std::regex token_re("[a-z][a-z][a-z]");
+//std::regex token_re("[a+b+c+d+e+f+g+h+i+j+k+l+m+n+o+p+q+r+s+t+u+v+w+x+y+z][a+b+c+d+e+f+g+h+i+j+k+l+m+n+o+p+q+r+s+t+u+v+w+x+y+z][a+b+c+d+e+f+g+h+i+j+k+l+m+n+o+p+q+r+s+t+u+v+w+x+y+z]");
+//std::regex token_re("[a|b|c|d|e|f|g|h|i|j|k|l|m|n|o|p|q|r|s|t|u|v|w|x|y|z][a|b|c|d|e|f|g|h|i|j|k|l|m|n|o|p|q|r|s|t|u|v|w|x|y|z][a|b|c|d|e|f|g|h|i|j|k|l|m|n|o|p|q|r|s|t|u|v|w|x|y|z]");	
+int main(){
+	std::string text = "qtv";
+    std::cout << std::boolalpha << std::regex_match(text, token_re);
+
+	return 0;
+}
