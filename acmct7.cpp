8b8f4611a329a5c621c28cddf371b6d30d93829a
KozakNazar <55637421+KozakNazar@users.noreply.github.com>
2024-03-07 06:04:47 +0200

    Update acmct7.cpp

--- acmct7.cpp
+++ acmct7.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <regex>
 
-/* Example for regular expression:
+/* An example of a regular expression for Low9:
 (a+b+c+d+e+f+g+h+i+j+k+l+m+n+o+p+q+r+s+t+u+v+w+x+y+z)(a+b+c+d+e+f+g+h+i+j+k+l+m+n+o+p+q+r+s+t+u+v+w+x+y+z)(a+b+c+d+e+f+g+h+i+j+k+l+m+n+o+p+q+r+s+t+u+v+w+x+y+z)
 */
 std::regex token_re("[a-z]{3}");
